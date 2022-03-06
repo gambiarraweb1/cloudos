4d780077b9a1f9fa5b6daf4828bd1c0ccc1c59ca
gambiarraweb1 <diegohot14@gmail.com>
2022-03-06 20:07:48 -0300

    0

--- application/models/Os_model.php
+++ application/models/Os_model.php
@@ -225,9 +225,8 @@ class Os_model extends CI_Model
 
                 $temp_array = array();
                 $temp_array['id'] = $row['idClientes'];
-                $temp_array['value'] = $row['nomeCliente'];
-                //$temp_array['label'] = "<img width='25' src='data:image/png;base64,' . '$row['foto'] />";// . $row['nomeCliente'] . '';
-                $temp_array['label'] = '<img src="data:image/png;base64,' . $row['foto'] . '" width="25" />&nbsp;&nbsp;&nbsp;' . $row['nomeCliente'] . '';
+                $temp_array['value'] = $row['nomeCliente'] . ' | CPF: ' . $row['documento'];
+                $temp_array['label'] = '<img src="data:image/png;base64,' . $row['foto'] . '" width="50" />&nbsp;&nbsp;&nbsp;' . $row['nomeCliente'] . ' | CPF: ' . $row['documento'] . '';
                 $row_set[] = $temp_array;
             }
             echo json_encode($row_set);
